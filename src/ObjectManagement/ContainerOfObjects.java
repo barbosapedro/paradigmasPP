dc581a3057f9791805c46c3c149fdb1f3711e41c
ANTONIO PEDRO FARIA BARBOSA <8180651@estg.ipp.pt>
2019-04-09 10:31:22 +0100

    Primeiro Commit

---
+++ src/ObjectManagement/ContainerOfObjects.java
@@ -0,0 +1,28 @@
+package ObjectManagement;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 8180651
+ */
+public class ContainerOfObjects {
+
+    
+    //metodo construtor
+    
+    //Valor máximo de elementos que ContainerOfObjects pode conter
+    
+    //Valor por defeito de elementos (tamanho omitido) que ContainerOfObjects pode conter
+    
+    //Instanciar com base num vetor de elementos recebidos
+    
+    //Operações de adicionar, remover, substituir e procurar objectos
+    
+    
+    
+}
