c525814fdc1964025b560dafe350b33653ed27fe
ANTONIO PEDRO FARIA BARBOSA <8180651@estg.ipp.pt>
2019-04-09 11:39:13 +0100

    Segundo Commit

--- src/ObjectManagement/ContainerOfObjects.java
+++ src/ObjectManagement/ContainerOfObjects.java
@@ -11,18 +11,78 @@ package ObjectManagement;
  * @author 8180651
  */
 public class ContainerOfObjects {
-
+    
+    private final int DEFAULT_SIZE = 100;
+    private Object objects[];
+    private int counter = 0;
     
     //metodo construtor
     
     //Valor máximo de elementos que ContainerOfObjects pode conter
-    
+    public ContainerOfObjects(int maxSize){
+        this.object = new Object[maxSize];
+    }
     //Valor por defeito de elementos (tamanho omitido) que ContainerOfObjects pode conter
-    
+    public ContainerOfObjects(Object[] object){
+        this.object = object;
+        this.counter = this.objects.length;
+    }
     //Instanciar com base num vetor de elementos recebidos
-    
+    public ContainerOfObjects(){
+        this.object = new Object[DEFAULT_SIZE];
+    }
     //Operações de adicionar, remover, substituir e procurar objectos
-    
-    
+    protected boolean addObject(Object newObject){
+        if(counter < object.length){
+            object[counter] = newObject;
+            counter +=1;
+            return true;
+        }else{
+            System.out.println("Array cheio.");
+            return false;
+        }
+    }
+    protected removeObject(int position){
+        if(position < object.length && position != -1){
+            if(object[position] == null){
+                System.out.println("Objecto nao existe");
+                return null;
+            }else{
+                Object delete = object[position];
+                object[position] = null;
+                for(int i = position; i <(counter-1); i++){
+                    object[i]=object[i + 1];
+                }
+                counter -= 1;
+                object[counter] = null;
+                return delete;
+            }
+        }else {
+            System.out.println("Posicao Inexistente");
+            return null;
+        }
+    }
+    protected boolean setObject(int position, Object newObject){
+        if(position < object.length){
+            if(object[position] == null){
+                System.out.println("Nao existe nenhum objecto nesta posicao");
+                return false;
+            }else {
+                object[position]= newObject;
+                return true;
+            }
+        }else {
+            System.out.println("Posicao Inexistente");
+            return false;
+        }
+    }
+    protected int findObject(Object obj){
+        for(int i = 0; i < this.object.length; i++){
+            if(this.object[i].equals(obj)){
+                return i;
+            }
+        }
+        return -1;
+    }
     
 }
